e996024c682c5f9c7aa409b9043f5d77433660d7
Bryan Yaggi <yaggi@tamu.edu>
2024-04-24 16:24:45 -0500

    Add Bullet videos, organize

--- sim_flopping.py
+++ sim_flopping.py
@@ -1,61 +1,62 @@
-#!/usr/bin/env python3
+#! /usr/bin/env python3
 
+import pybullet as p
+import time
+import pybullet_data
 import math
 import numpy as np
-import os
-import roboticstoolbox as rtb
-from roboticstoolbox.robot.Robot import Robot
-import spatialmath as sm
-import spatialgeometry as sg
-import swift
 
-class Manipulator(Robot):
+def configureEnvironment(gravityDown=True):
+    p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0) # hide panes
+    cameraSpecs = p.getDebugVisualizerCamera()
+    p.resetDebugVisualizerCamera(cameraDistance=1.0, cameraYaw=cameraSpecs[-4], cameraPitch=cameraSpecs[-3],
+            cameraTargetPosition=cameraSpecs[-1]) # zoom in
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())
+    p.loadURDF('plane.urdf')
+    p.setGravity(0, 0, -9.81)
+    if not gravityDown:
+        p.setGravity(0, 0, 9.81)
+
+class Robot:
     def __init__(self):
-        path = os.getcwd()
-        path += '/Manipulator/urdf/Manipulator.urdf'
-        links, name, urdf_string, urdf_filepath = self.URDF_read(path)
-        super().__init__(links, name=name, urdf_string=urdf_string,
-                urdf_filepath=urdf_filepath)
-        self.manufacturer = 'Bryan Yaggi'
+        self.urdf = 'Manipulator/urdf/ManipulatorMod.urdf'
 
-        self.qz = np.zeros(7)
-        self.addconfiguration_attr('qz', self.qz)
-
-def flop(gravityDown=True):
-    robot = Manipulator()
-
-    env = swift.Swift()
-    env.launch(realtime=True)
-    env.add(robot)
-
-def test():
-    #robot = rtb.models.URDF.UR10()
-    #robot = rtb.models.URDF.Puma560()
-    robot = rtb.models.DH.Puma560()
-    q = np.zeros(6)
-    M = robot.inertia(q)
-    print(M)
-
-def control():
-    env = swift.Swift()
-    env.launch(realtime=True)
-
-    robot = rtb.models.Panda()
-    robot.q = robot.qr # pre-defined 'ready' configuration
-
-    Tep = robot.fkine(robot.q) * sm.SE3.Trans(0.2, 0.2, 0.45) # target end effector pose
-    axes = sg.Axes(length=0.1, pose=Tep)
-    env.add(axes)
-
-    arrived = False
-    dt = 0.05
-    env.add(robot)
-
-    while not arrived:
-        v, arrived = rtb.p_servo(robot.fkine(robot.q), Tep, gain=1, threshold=0.01) # v is desired velocity
-        robot.qd = np.linalg.pinv(robot.jacobe(robot.q)) @ v # qd is joint velocity vector
-        env.step(dt)
+    def loadRobot(self):
+        self.id = p.loadURDF(self.urdf, useFixedBase=True)
+        self.n = p.getNumJoints(self.id)
+        
+        # Disable default velocity controllers
+        p.setJointMotorControlArray(self.id, list(range(self.n)), p.VELOCITY_CONTROL, forces=[0.] * self.n)
     
+    def setInitialConfiguration(self):
+        angle = 10 * math.pi / 180
+        for i in range(self.n):
+            p.resetJointState(self.id, i, targetValue=angle)
+
+    def flop(self, gravityDown=True):
+        if gravityDown:
+            p.connect(p.GUI, options='--mp4=downflop_bullet.mp4')
+        else:
+            p.connect(p.GUI, options='--mp4=upflop_bullet.mp4')
+        configureEnvironment(gravityDown)
+        self.loadRobot()
+        self.setInitialConfiguration()
+
+        time.sleep(5)
+        for i in range(1000):
+            p.stepSimulation()
+            time.sleep(1 / 240)
+        
+        p.disconnect()
+
+def flopDown():
+    robot = Robot()
+    robot.flop()
+
+def flopUp():
+    robot = Robot()
+    robot.flop(False)
+
 if __name__ == '__main__':
-    #flop()
-    test()
+    flopDown()
+    #flopUp()
